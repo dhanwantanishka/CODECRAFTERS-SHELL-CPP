92b7b2847c3568e903beeb819fdf35e134a19fc9
dhanwantanishka <dhanwantanishka4169@gmail.com>
2025-06-14 22:23:46 +0530

    [This is an implementation of PWD builtin command]

--- src/main.cpp
+++ src/main.cpp
@@ -1,70 +1,56 @@
-#include <iostream>
-#include <string>
-#include <unordered_set>
-#include <filesystem>
+#include <bits/stdc++.h>
+#include "command.h"
+#include "commands.h"
+#include "error.h"
+using namespace std;
 
-std::string cmd_in_path(const std::string& cmd) ;
-
-int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-  std::unordered_set<std::string> cmds {"exit", "echo", "type"};
-  // Uncomment this block to pass the first stage
-  while (true)
+vector<string> split(string input)
+{
+  vector<string> v;
+  string s;
+  int n = input.size();
+  for (int i = 0; i < n; i++)
   {
-    std::cout << "$ ";
-
-    std::string input, cmd, args, cmd_path;
-    std::getline(std::cin, input);
-    cmd = input.substr(0, input.find_first_of(' '));
-    args = input.substr(input.find_first_of(' ') + 1);
-
-    if(cmd == "exit") {
-      exit(stoi(args));
+    if (input[i] == ' ')
+    {
+      v.push_back(s);
+      s.clear();
     }
-    else if(cmd == "echo") {
-      std::cout << args << std::endl;
-    }
-    else if(cmd == "type") {
-      if(cmds.find(args) != cmds.end()) {
-        std::cout << args << " is a shell builtin" << std::endl;
-      }
-      else if(cmd_path = cmd_in_path(args); !cmd_path.empty()) {
-        std::cout << args << " is " << cmd_path << std::endl;
-      }
-      else {
-        std::cout << args << ": not found"<< std::endl;
-      }
-    }
-    else if(cmd_path = cmd_in_path(cmd); !cmd_path.empty()) {
-        int res = system(input.c_str());
-    }
-    else {
-      std::cout << input << ": command not found" << std::endl;
+    else
+    {
+      s.push_back(input[i]);
     }
   }
+  if (!s.empty())
+  {
+    v.push_back(s);
+  }
+  return v;
 }
 
-std::string cmd_in_path(const std::string& cmd) {
-  std::string path = getenv("PATH");
-  std::string search_dir;
-  int left_occ = 0;
+int main()
+{
+  // Flush after every cout / std:cerr
+  cout << unitbuf;
+  cerr << unitbuf;
 
-  for(int i = 0; i < path.size(); i++) {
-    if(path[i] == ':') {
-      search_dir = path.substr(left_occ, i - left_occ);
-      if(std::filesystem::exists(search_dir + '/' + cmd)) {
-        return search_dir + '/' + cmd;
-      }
-      left_occ = i + 1;
+  command *bash = new command();
+  bash->addCommand("echo", echo);
+  bash->addCommand("exit", myExit);
+  bash->addCommand("type", type);
+  bash->addCommand("pwd", pwd);
+  while (1)
+  {
+    cout << "$ ";
+    string input;
+    getline(cin, input);
+    vector<string> v = split(input);
+    switch (bash->executeCommand(v))
+    {
+    case 0:
+      error(v);
+      break;
     }
   }
-
-  search_dir = path.substr(path.find_last_of(':'));
-  if(std::filesystem::exists(search_dir + '/' + cmd)) {
-        return search_dir + '/' + cmd;
-  }
-
-  return "";
+  return 0;
 }
