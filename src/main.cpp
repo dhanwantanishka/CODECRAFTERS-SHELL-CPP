0ee515bf21694e71e65c613848f5d49f7d1a4041
dhanwantanishka <dhanwantanishka4169@gmail.com>
2025-06-14 22:11:37 +0530

    [This is an implementation of type builtin command for the shell ]

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
+#include <string>
+#include <cstring>
+
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  
   while (true) {
     std::string input;
+
     std::cout << "$ ";
     std::getline(std::cin, input);
-    if (input == "exit 0") return 0;
-    else if (input.find("echo") != std::string::npos) std::cout << input.substr(5) << "\n";
-    else std::cout << input << ": command not found\n";
+
+    if (input == "exit 0") {
+      break;
+    } else if (input.substr(0, 4) == "echo") {
+        std::cout << input.substr(5) << std::endl;
+    } else if (input.substr(0, 7) == "invalid") {
+        std::cout << input + ": command not found" << std::endl;
+    } else if (input.substr(0, 9) == "type echo" || input.substr(0, 9) == "type exit" || input.substr(0, 9) == "type type") {
+        std::cout << input.substr(5) + " is a shell builtin" << std::endl;
+    } else {
+        std::cout << input.substr(5) + ": not found" << std::endl;
+    }
   }
-  return 0; 
+  return 0;
 }
