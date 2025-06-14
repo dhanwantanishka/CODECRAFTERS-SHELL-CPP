6d6943d8b2dd7d56b924251275734df91b18cad3
dhanwantanishka <dhanwantanishka4169@gmail.com>
2025-06-14 22:03:37 +0530

    [This is an implementation of exit builtin]

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  // Flush after every cout / std:cerr
+  cout << unitbuf;
+  cerr << unitbuf;
 
   // Uncomment this block to pass the first stage
-  std::cout << "$ ";
+  cout << "$ ";
 
-  std::string input;
-  std::getline(std::cin, input);
-  std::cout << input << ": command not found" << std::endl;
+  string input;
+  getline(cin, input);
+  if (input == "exit 0") exit(0);  
+  cout << input << ": command not found" << endl;
   main();
 }
