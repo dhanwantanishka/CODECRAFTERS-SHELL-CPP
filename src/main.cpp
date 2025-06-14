603dd5a6cffb00f6c10c826f26406cd821b16d15
dhanwantanishka <dhanwantanishka4169@gmail.com>
2025-06-14 22:07:18 +0530

    [This is an implementation of echo builtin command ]

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
-#include <string>
-
-using namespace std;
 
 int main() {
-  // Flush after every cout / std:cerr
-  cout << unitbuf;
-  cerr << unitbuf;
+  // Flush after every std::cout / std:cerr
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
 
   // Uncomment this block to pass the first stage
-  cout << "$ ";
-
-  string input;
-  getline(cin, input);
-  if (input == "exit 0") exit(0);  
-  cout << input << ": command not found" << endl;
-  main();
+  
+  while (true) {
+    std::string input;
+    std::cout << "$ ";
+    std::getline(std::cin, input);
+    if (input == "exit 0") return 0;
+    else if (input.find("echo") != std::string::npos) std::cout << input.substr(5) << "\n";
+    else std::cout << input << ": command not found\n";
+  }
+  return 0; 
 }
